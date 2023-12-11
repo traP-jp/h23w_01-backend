d2fcf4b08372ebe80a88b1430d1722eb0601c000
itt828 <iittt828@gmail.com>
2023-12-12 00:59:47 +0900

    :sparkles: Make relation

--- entity/src/card.rs
+++ entity/src/card.rs
@@ -16,6 +16,15 @@ pub struct Model {
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(has_many = "super::publish_channel::Entity")]
+    PublishChannel,
+}
+
+impl Related<super::publish_channel::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::PublishChannel.def()
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {}
--- entity/src/publish_channel.rs
+++ entity/src/publish_channel.rs
@@ -16,6 +16,19 @@ pub struct Model {
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::card::Entity",
+        from = "Column::CardId",
+        to = "super::card::Column::Id"
+    )]
+    Card,
+}
+
+impl Related<super::card::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Card.def()
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {}
