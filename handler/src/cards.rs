5fc24b10e4b2bbef28a162aa923689dbe7e67d46
H1rono <hronok66@gmail.com>
2023-12-13 18:04:28 +0900

    :sparkles: Add card content API

--- handler/src/cards.rs
+++ handler/src/cards.rs
@@ -1,3 +1,4 @@
+use rocket::fs::NamedFile;
 use rocket::http::Status;
 use rocket::serde::json::Json;
 use rocket::Route;
@@ -76,6 +77,59 @@ pub async fn delete_one(id: String) -> Status {
     Status::NoContent
 }
 
+const CARD_ID: &str = "89d136ad-1ba2-4974-a44a-cc9b5c8c0670";
+
+#[rocket::get("/<id>/svg")]
+pub async fn get_svg(id: String) -> (Status, Option<NamedFile>) {
+    println!("get image.svg {}", id);
+    if id != CARD_ID {
+        return (Status::NotFound, None);
+    }
+    (
+        Status::Ok,
+        NamedFile::open("./mock-assets/a.svg").await.ok(),
+    )
+}
+
+#[rocket::post("/<id>/svg", data = "<svg>")]
+pub async fn post_svg(svg: Vec<u8>, id: String) -> Status {
+    println!("post image.svg {} with size {}", id, svg.len());
+    Status::NoContent
+}
+
+#[rocket::patch("/<id>/svg", data = "<svg>")]
+pub async fn patch_svg(svg: Vec<u8>, id: String) -> Status {
+    println!("patch image.svg {} with size {}", id, svg.len());
+    Status::NoContent
+}
+
+#[rocket::get("/<id>/png")]
+pub async fn get_png(id: String) -> (Status, Option<NamedFile>) {
+    println!("get image.png {}", id);
+    if id != CARD_ID {
+        return (Status::NotFound, None);
+    }
+    (
+        Status::Ok,
+        NamedFile::open("./mock-assets/a.png").await.ok(),
+    )
+}
+
+#[rocket::post("/<id>/png", data = "<png>")]
+pub async fn post_png(png: Vec<u8>, id: String) -> Status {
+    println!("post image.png {} with size {}", id, png.len());
+    Status::NoContent
+}
+
+#[rocket::patch("/<id>/png", data = "<png>")]
+pub async fn patch_png(png: Vec<u8>, id: String) -> Status {
+    println!("patch image.png {} with size {}", id, png.len());
+    Status::NoContent
+}
+
 pub fn routes() -> Vec<Route> {
-    rocket::routes![get_all, post, get_mine, get_one, delete_one]
+    rocket::routes![
+        get_all, post, get_mine, get_one, delete_one, get_svg, post_svg, patch_svg, get_png,
+        post_png, patch_png
+    ]
 }
