109044eac478582d20249148cab33098b56dedf1
H1rono <hronok66@gmail.com>
2023-12-13 21:31:04 +0900

    :sparkles: Add traQ API

--- handler/src/main.rs
+++ handler/src/main.rs
@@ -5,6 +5,8 @@ use std::env;
 
 use traq_bot_http::RequestParser;
 
+use bot_client::BotClient;
+
 #[get("/ping")]
 fn ping() -> &'static str {
     "pong"
@@ -12,12 +14,19 @@ fn ping() -> &'static str {
 
 #[launch]
 async fn rocket() -> _ {
-    let verification_token = env::var("VERIFICATION_TOKEN").unwrap();
+    let verification_token =
+        env::var("VERIFICATION_TOKEN").expect("env var VERIFICATION_TOKEN is unset");
+    let access_token = env::var("BOT_ACCESS_TOKEN").expect("env var BOT_ACCESS_TOKEN is unset");
     let parser = RequestParser::new(&verification_token);
+    let client = BotClient::new(access_token);
     rocket::build()
         .mount("/api", routes![ping])
         .mount("/api/cards", handler::cards::routes())
         .mount("/api/images", handler::images::routes())
         .mount("/bot", routes![handler::bot::bot_event])
+        .mount("/api/stamps", handler::traq_api::stamps::routes())
+        .mount("/api/users", handler::traq_api::users::routes())
+        .mount("/api/channels", handler::traq_api::channels::routes())
         .manage(parser)
+        .manage(client)
 }
