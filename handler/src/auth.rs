399ae86630784b7cf6bede76b6613d6e84ca5d82
H1rono <hronok66@gmail.com>
2023-12-15 00:51:45 +0900

    :sparkles: Add AuthUser config

--- handler/src/auth.rs
+++ handler/src/auth.rs
@@ -2,9 +2,24 @@ use rocket::async_trait;
 use rocket::http::Status;
 use rocket::request::{FromRequest, Outcome, Request};
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct AuthUserConfig(pub bool);
+
+impl From<bool> for AuthUserConfig {
+    fn from(value: bool) -> Self {
+        Self(value)
+    }
+}
+
+impl From<AuthUserConfig> for bool {
+    fn from(value: AuthUserConfig) -> Self {
+        value.0
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct AuthUser<'r> {
-    pub id: &'r str,
+    pub id: Option<&'r str>,
 }
 
 #[async_trait]
@@ -12,10 +27,17 @@ impl<'r> FromRequest<'r> for AuthUser<'r> {
     type Error = ();
 
     async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let auth = req
+            .rocket()
+            .state::<AuthUserConfig>()
+            .map(|c| c.0)
+            .unwrap_or(true);
+        if !auth {
+            return Outcome::Success(AuthUser { id: None });
+        }
         let Some(id) = req.headers().get_one("X-Forwarded-User") else {
             return Outcome::Error((Status::Unauthorized, ()));
         };
-
-        Outcome::Success(AuthUser { id })
+        Outcome::Success(AuthUser { id: Some(id) })
     }
 }
