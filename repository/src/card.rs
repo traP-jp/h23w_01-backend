fe95440f42700c0f1990b46420e254b903332d13
itt828 <iittt828@gmail.com>
2023-12-14 12:22:29 +0900

    :bug: Fix card repository

--- repository/src/card.rs
+++ repository/src/card.rs
@@ -1,45 +1,83 @@
-use entity::prelude::*;
-use sea_orm::{prelude::DateTimeLocal, DatabaseConnection, DbErr};
+use entity::{prelude::*, *};
+use sea_orm::{
+    prelude::DateTimeLocal, ActiveValue, ColumnTrait, DatabaseConnection, DbErr, EntityTrait,
+    QueryFilter, TransactionTrait,
+};
 use uuid::Uuid;
 
 #[async_trait::async_trait]
 pub trait CardRepository {
-    async fn insert_card(&self, card: &Card) -> Result<Card, DbErr>;
-    async fn get_all_cards(&self) -> Result<Vec<Card>, DbErr>;
-    async fn get_my_cards(&self, user_id: Uuid) -> Result<Vec<Card>, DbErr>;
-    async fn get_card_by_id(&self, card_id: Uuid) -> Result<Card, DbErr>;
+    async fn save_card(&self, params: &SaveCardParams) -> Result<(), DbErr>;
+    async fn get_all_cards(&self) -> Result<Vec<card::Model>, DbErr>;
+    async fn get_my_cards(&self, user_id: Uuid) -> Result<Vec<card::Model>, DbErr>;
+    async fn get_card_by_id(&self, card_id: Uuid) -> Result<card::Model, DbErr>;
     async fn delete_card(&self, card_id: Uuid) -> Result<(), DbErr>;
 }
 
 pub struct CardRepositoryImpl(DatabaseConnection);
 
+#[async_trait::async_trait]
 impl CardRepository for CardRepositoryImpl {
-    async fn get_all_cards(&self) -> Result<Vec<Card>> {
+    async fn save_card(&self, params: &SaveCardParams) -> Result<(), DbErr> {
+        // TODO: 画像の保存
+        let db = &self.0;
+        let tx = db.begin().await.unwrap();
+        let card = card::ActiveModel {
+            id: ActiveValue::Set(params.id),
+            owner_id: ActiveValue::Set(params.owner_id),
+            publish_date: ActiveValue::Set(params.publish_date.naive_local()),
+            message: ActiveValue::Set(params.message.clone()),
+        };
+        let channels = params
+            .channels
+            .iter()
+            .map(|channel_id| publish_channel::ActiveModel {
+                id: ActiveValue::Set(*channel_id),
+                card_id: ActiveValue::Set(card.id.clone().unwrap()),
+            })
+            .collect::<Vec<_>>();
+        Card::insert(card).exec(&tx).await.unwrap();
+        PublishChannel::insert_many(channels)
+            .exec(&tx)
+            .await
+            .unwrap();
+
+        tx.commit().await.unwrap();
+        Ok(())
+    }
+    async fn get_all_cards(&self) -> Result<Vec<card::Model>, DbErr> {
         let db = &self.0;
         let cards = Card::find().all(db).await?;
         Ok(cards)
     }
-    async fn insert_card(&self, card: &Card) -> Result<Card> {
-        let db = &self.0;
-        let card = card.clone().insert(db).await?;
-        Ok(card)
-    }
-    async fn get_my_cards(&self, user_id: Uuid) -> Result<Vec<Card>> {
+
+    async fn get_my_cards(&self, user_id: Uuid) -> Result<Vec<card::Model>, DbErr> {
         let db = &self.0;
         let cards = Card::find()
-            .filter(card::Column::UserId.contains(user_id))
+            .filter(card::Column::Id.contains(user_id))
             .all(db)
             .await?;
         Ok(cards)
     }
-    async fn get_card_by_id(&self, card_id: Uuid) -> Result<Card> {
+    async fn get_card_by_id(&self, card_id: Uuid) -> Result<card::Model, DbErr> {
         let db = &self.0;
-        let card = Card::find_by_id(card_id).one(db).await?;
+        let card = Card::find_by_id(card_id)
+            .one(db)
+            .await?
+            .ok_or(DbErr::RecordNotFound("card not found".to_string()))?;
         Ok(card)
     }
     async fn delete_card(&self, card_id: Uuid) -> Result<(), DbErr> {
         let db = &self.0;
-        Card::find_by_id(card_id).delete(db).await?;
+        Card::delete_by_id(card_id).exec(db).await?;
         Ok(())
     }
+}
+
+pub struct SaveCardParams {
+    pub id: Uuid,
+    pub owner_id: Uuid,
+    pub publish_date: DateTimeLocal,
+    pub message: Option<String>,
+    pub channels: Vec<Uuid>,
 }
