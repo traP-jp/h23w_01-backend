d227880a8ea9270b9e403ef55097d0fefbe28c24
itt828 <iittt828@gmail.com>
2023-12-16 17:23:56 +0900

    :fire: Omake

--- entity/src/card_svg.rs
+++ entity/src/card_svg.rs
@@ -6,7 +6,6 @@ use serde::{Deserialize, Serialize};
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
     pub card_id: Uuid,
-    pub content: String,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
